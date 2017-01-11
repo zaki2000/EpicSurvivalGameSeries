9aa595643ad13e62430ff10cf670f7d274a9c5c8
DESKTOP-SA0ULII\zaki <zhang.kai.2012.04.14@gmail.com>
2017-01-11 19:55:28 +0800

    2017/1/11 by zaki 提交武器 维持原设计的武器和拾取
    提交AI
    怪物可以搜索所有非同势力的角色

---
+++ SurvivalGame/Source/SurvivalGame/Private/Items/SWeaponBase_BP.cpp
@@ -0,0 +1,44 @@
+// Copyright 1998-2015 Epic Games, Inc. All Rights Reserved.
+
+#include "SurvivalGame.h"
+#include "SWeaponBase_BP.h"
+#include "Runtime/Engine/Classes/Kismet/KismetMathLibrary.h"
+
+
+void ASWeaponBase_BP::FireWeapon()
+{
+	const FVector AimDir = GetAdjustedAim();
+	const FVector CameraPos = GetCameraDamageStartLocation(AimDir);
+	const FVector EndPos = CameraPos + (AimDir * ShootRange);
+	const FRotator rotator = UKismetMathLibrary::FindLookAtRotation(CameraPos, EndPos);
+	BP_Fire(CameraPos, rotator, ShootRange, EndPos);
+}
+
+float ASWeaponBase_BP::GetDamagePreHit() const
+{
+	return DamagePreHit;
+}
+
+float ASWeaponBase_BP::GetShootRange() const
+{
+	return ShootRange;
+}
+
+int32 ASWeaponBase_BP::GetShootInterval() const
+{
+	return ShootInterval;
+}
+
+TSubclassOf<AActor> ASWeaponBase_BP::GetProjectileClass() const
+{
+	return ProjectileClass;
+}
+
+void ASWeaponBase_BP::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const
+{
+	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+	DOREPLIFETIME(ASWeaponBase_BP, DamagePreHit);
+	DOREPLIFETIME(ASWeaponBase_BP, ShootRange);
+	DOREPLIFETIME(ASWeaponBase_BP, ShootInterval);
+	DOREPLIFETIME(ASWeaponBase_BP, ProjectileClass);
+}
---
+++ SurvivalGame/Source/SurvivalGame/Public/Items/SWeaponBase_BP.h
@@ -0,0 +1,45 @@
+// Copyright 1998-2015 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "Items/SWeapon.h"
+#include "SWeaponBase_BP.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SURVIVALGAME_API ASWeaponBase_BP : public ASWeapon
+{
+	GENERATED_BODY()
+	
+public:
+	virtual void FireWeapon() override;
+
+	UPROPERTY(EditAnywhere, Replicated, Category = "Weapon")
+	float DamagePreHit;
+
+	UFUNCTION(BlueprintCallable, Category = "Weapon")
+	float GetDamagePreHit() const;
+
+	UPROPERTY(EditAnywhere, Replicated, Category = "Weapon")
+	float ShootRange;
+
+	UFUNCTION(BlueprintCallable, Category = "Weapon")
+	float GetShootRange() const;
+
+	UPROPERTY(EditAnywhere, Replicated, Category = "Weapon")
+	int32 ShootInterval;
+
+	UFUNCTION(BlueprintCallable, Category = "Weapon")
+	int32 GetShootInterval() const;
+
+	UPROPERTY(EditAnywhere, Replicated, Category = "Weapon")
+	TSubclassOf<AActor> ProjectileClass;
+
+	UFUNCTION(BlueprintCallable, Category = "Weapon")
+	TSubclassOf<AActor> GetProjectileClass() const;
+
+	UFUNCTION(BlueprintImplementableEvent, BlueprintCallable, Category = "Weapon")
+	void BP_Fire(struct FVector const& FromLocation, struct FRotator const& Rotator, float Range, struct FVector const& TargetLocation);
+};
